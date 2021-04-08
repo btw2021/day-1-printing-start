7314639201a9302522053f20ee99ab4ba2676f94
behailu2021 <>
2021-04-08 14:45:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+# ----Write your code below this line 👇
+# 1. --the print() fuction
+# print("Hello, world!")
+# print("Day 1 - String Manipulation")
+#print("String Concatination is done with the '+ ' sign.")
+#print(" example print('Hello' + 'world!'")
+#print("New lines can be created with a backslash and n.")
+
+#2.   the input() function
+#input("What is your name?")
+#print("Hello " + input("What is your name?"))
+
+# assignment-----Write a program that prints the number of characters in a user's name. 
+#input("What is your name? ")
+
+#print( len( input("What is your name? ") ) )
+
+# Variables
+
+#name = input("what is your name? ")
+#print(name)
+#-- examples
+#name = input("What is your name? ")
+#length = len(name)
+#print(length)
+# challenge change places 
+#a = input("a:")
+#b = input("b:")
+#c = a
+#a = b
+#b = c
+
+# Project 1: the Band name GeneratorExit
+
+#1. Create a greeting for your program.
+#print("Hello, welcome to the band name generator!")
+
+#2. Ask the user for the city that they grew up in.
+
+#city = input("Which city did you grow up in?\n ")
+
+
+#3. Ask the user for the name of a pet.
+
+#pet = input("What is the name of your pet?\n ")
+
+
+#4. Combine the name of their city and pet and show them their band name.
+#print("Your band name could be " + city + " " + pet)
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://band-name-generator-end.appbrewery.repl.run/
+# use \n
+
